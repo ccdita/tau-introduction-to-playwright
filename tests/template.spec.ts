95c2e4dce9975ea176956781258f16a688d2fb3c
ccdita <christaldita@gmail.com>
2026-04-29 16:46:39 -0400

    Add Ch3.1 notes

--- tests/template.spec.ts
+++ tests/template.spec.ts
@@ -1,30 +1,46 @@
+/**
+ * Non-functional template with guidance on how to structure test files
+ */
 import { test, expect } from '@playwright/test';
 
 //AAA Pattern
-
+// Have all Arranges at the beginning, then Acts, and finally, Asserts
+// Can be nested (e.g., Arrange section has Arranges, Acts, and Asserts in it)
 // [Arrange]
 // [Act]
 // [Assert]
 
+// ========== ARRANGE SECTION ==========
+
+// Tip: variables should come at the very beginning of a file or method
 const password = process.env.PASSWORD;
 
+/**
+ * .beforeAll() executes before all tests within the file 
+ * Common scenarios:
+ * - Start a server
+ * - Create a db connection
+ * - Reuse a sign-in state
+ */
 test.beforeAll(async ({ playwright }) => {
+    // Skip the test if the environment is production
     test.skip(
       !!process.env.PROD,
       'Test intentionally skipped in production due to data dependency.'
     );
-    // start a server
-    // create a db connection
-    // reuse a sign in state
 });
   
+/**
+ * .beforeEach() executes before each test within the file
+ * Common scenarios:
+ * - Open a URL before each test
+ * - Clean up the db
+ * - Create a page object
+ * - Dismiss a modal
+ * - Load parameters
+ */
 test.beforeEach(async ({ page }, testInfo) => {
     console.log(`Running ${testInfo.title}`);
-    // open a URL
-    // clean up the DB
-    // create a page object
-    // dismiss a modal
-    // load params
 });
 
 test.afterAll(async ({ page }, testInfo) => {
@@ -32,7 +48,12 @@ test.afterAll(async ({ page }, testInfo) => {
     // close a DB connection
 });
 
+/**
+ * .afterEach() executes after each test within the file
+ * Example scenario: close a db connection
+ */
 test.afterEach( async ({ page }, testInfo) => {
+    // Log each test result within this file
     console.log(`Finished ${testInfo.title} with status ${testInfo.status}`);
 
     if (testInfo.status !== testInfo.expectedStatus)
@@ -40,11 +61,20 @@ test.afterEach( async ({ page }, testInfo) => {
     // clean up all the data we created for this test through API calls
 });
 
-// test.describe('Test Case', () => {
-// test.describe.only('Test Case', () => {
-test.describe.skip('Test Case', () => {
-    test('Test Scenario One', async ({ page }) => {
+// ====================
+
+/**
+ * .describe() groups tests together
+ * One file can have multiple describes (instructor recommends splitting the describes between files)
+ */
+// test.describe('Test Scenario', () => {
+// test.describe.only('Test Scenario', () => {
+test.describe.skip('Test Scenario', () => {
+    test('Test Case One', async ({ page }) => {
+        // Steps can be used as Arrange, Act, and Assert, if needed
+        // Can also use .only() or .skip() here
         await test.step('Step One', async () => {
+            // Can further have AAA implementations here
             // ...
         });
 
@@ -55,20 +85,20 @@ test.describe.skip('Test Case', () => {
         // ...
     });
   
-    test('Test Scenario Two', async ({ page }) => {
+    test('Test Case Two', async ({ page }) => {
         // Arrange
         // Act
         // Assert
     });
 /**
-    test.only('Test Scenario Three', async ({ page }) => {
+    test.only('Test Case Three', async ({ page }) => {
         // Arrange
         // Act
         // Assert
     });
  */  
 /**
-    test.skip('Test Scenario Four', async ({ page }) => {
+    test.skip('Test Case Four', async ({ page }) => {
         // Arrange
         // Act
         // Assert
